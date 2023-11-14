41a525f9cd0a407bb6aa12ea12d31f10df4c9772
Brandon Seegraves <brandonseegraves@gmail.com>
2023-11-14 15:17:04 -0500

    first commit

---
+++ src/main/java/com/example/tictactoegame/Controller.java
@@ -0,0 +1,92 @@
+package com.example.tictactoegame;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class SceneController {
+
+    @FXML
+    private Button b1;
+
+    @FXML
+    private Button b2;
+
+    @FXML
+    private Button b3;
+
+    @FXML
+    private Button b4;
+
+    @FXML
+    private Button b5;
+
+    @FXML
+    private Button b6;
+
+    @FXML
+    private Button b7;
+
+    @FXML
+    private Button b8;
+
+    @FXML
+    private Button b9;
+
+    @FXML
+    private Button b10;
+
+    @FXML
+    private Button b11;
+
+    @FXML
+    private Button b12;
+
+    @FXML
+    private Button b13;
+
+    @FXML
+    private Button b14;
+
+    @FXML
+    private Button b15;
+
+    @FXML
+    private Button b16;
+
+    @FXML
+    private Button b17;
+
+    @FXML
+    private Button b18;
+
+    @FXML
+    private Button b19;
+
+    @FXML
+    private Button b20;
+
+    @FXML
+    private Button b21;
+
+    @FXML
+    private Button b22;
+
+    @FXML
+    private Button b23;
+
+    @FXML
+    private Button b24;
+
+    @FXML
+    private Button b25;
+
+    @FXML
+    private Label resultText;
+
+
+    public void disableButton(ActionEvent event) {
+        b1.setFocusTraversable(false);
+    }
+}
